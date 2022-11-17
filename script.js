2f2076a8138591c57bd087355296edefd043cbdb
Diogo Manhaes <dsmanhaes@gmail.com>
2022-11-17 12:16:19 -0300

    feat: added javascript behaviours

---
+++ script.js
@@ -0,0 +1,13 @@
+function handleClick() {
+    document.querySelector('#titulo').innerHTML = 'Saitama';
+}
+
+function handleMove() {
+    document.querySelector('#titulo').innerHTML = 'Saitama';
+}
+
+function handleOut() {
+    document.querySelector('#titulo').innerHTML = 'Diogo Manhães';
+}
+
+console.log('Estou aqui!!');
